d90fb1ceb26c3e33027d749c2b7ec11ff6aaa09e
Oystein Bjerland <oystein.bjerland@gmail.com>
2018-10-16 20:08:01 +0200

    fix space

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -2,78 +2,81 @@ import Network from './Network';
 
 
 class TestScene extends Phaser.Scene {
-    player: any;
-    squares: any;
-    cursors: any;
-    bomb: Phaser.GameObjects.Sprite;
-    network: Network;
-    enemies: Phaser.GameObjects.Sprite[];
+  player: any;
+  squares: any;
+  cursors: any;
+  bomb: Phaser.GameObjects.Sprite;
+  network: Network;
+  enemies: Phaser.GameObjects.Sprite[];
+  spacebar: any;
 
-    constructor() {
-        super({
-            key: 'TestScene'
-        });
+  constructor() {
+    super({
+      key: 'TestScene'
+    });
 
-        this.network = new Network();
+    this.network = new Network();
+  }
+
+  preload() {
+    this.load.image('player', '/assets/sprites/player.png');
+    this.load.image('black-square', '/assets/sprites/black-square.png');
+    this.load.image('bomb', '/assets/sprites/bomb.png');
+
+  }
+
+  create() {
+    this.player = this.physics.add.sprite(100, 100, 'player');
+    this.player.setCollideWorldBounds(true);
+    this.player.enableBody(true);
+    this.physics.world.enable(this.player);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    this.squares = this.physics.add.staticGroup();
+    for (var i = 70; i < 400; i += 80) {
+      for (var j = 80; j < 600; j += 80) {
+        this.squares.create(j, i, 'black-square');
+      }
     }
 
-    preload() {
-        this.load.image('player', '/assets/sprites/player.png');
-        this.load.image('black-square', '/assets/sprites/black-square.png');
-        this.load.image('bomb', '/assets/sprites/bomb.png');
+    this.bomb = this.add.sprite(100, 100, 'bomb');
+    this.bomb.visible = false;
+
+    this.enemies = [];
+    for (let i = 0; i < 10; i++) {
+      this.enemies[i] = this.add.sprite(0, 0, 'player');
+      this.enemies[i].visible = false;
 
     }
+  }
 
-    create() {
-        this.player = this.physics.add.sprite(100, 100, 'player');
-        this.player.setCollideWorldBounds(true);
-        this.player.enableBody(true);
-        this.physics.world.enable(this.player);
+  update(time: number, delta: number) {
+    this.physics.add.collider(this.player, this.squares);
 
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.squares = this.physics.add.staticGroup();
-        for (var i = 70; i < 400; i += 80) {
-            for (var j = 80; j < 600; j += 80) {
-                this.squares.create(j, i, 'black-square');
-            }
-        }
+    this.network.update(time, this.player, this.enemies);
 
-        this.bomb = this.add.sprite(100, 100, 'bomb');
-        this.bomb.visible = false;
-
-        this.enemies = [];
-        for (let i = 0; i < 10; i++) {
-            this.enemies[i] = this.add.sprite(0, 0, 'player');
-            this.enemies[i].visible=false;
-
-        }
+    if (this.cursors.left.isDown) {
+      this.player.x -= 5;
+    }
+    if (this.cursors.right.isDown) {
+      this.player.x += 5;
+    }
+    if (this.cursors.down.isDown) {
+      this.player.y += 5;
+    }
+    if (this.cursors.up.isDown) {
+      this.player.y -= 5;
     }
 
-    update(time: number, delta: number) {
-        this.physics.add.collider(this.player, this.squares);
-
-        this.network.update(time, this.player, this.enemies);
-
-        if (this.cursors.left.isDown) {
-            this.player.x -= 5;
-        }
-        if (this.cursors.right.isDown) {
-            this.player.x += 5;
-        }
-        if (this.cursors.down.isDown) {
-            this.player.y += 5;
-        }
-        if (this.cursors.up.isDown) {
-            this.player.y -= 5;
-        }
-
-        if (this.cursors.space.isDown) {
-            this.bomb.visible = true;
-            this.bomb.x = this.player.x;
-            this.bomb.y = this.player.y;
-            console.log('DROP BOMB');
-        }
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+      this.bomb.visible = true;
+      this.bomb.x = this.player.x;
+      this.bomb.y = this.player.y;
+      console.log('DROP BOMB');
     }
+  }
 }
 
 export default TestScene;
