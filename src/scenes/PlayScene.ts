b20defdb53f9ab39ce9e59b7dec49dfd192a00e4
moddaman <ovrebo90@gmail.com>
2018-11-02 17:29:23 +0100

    Manbomber er i egen fil

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,14 +1,16 @@
 import Network from './Network';
-
+import { Manbomber } from "../objects/Manbomber";
 
 class TestScene extends Phaser.Scene {
-  player: any;
+  private player: Manbomber;
   squares: any;
   cursors: any;
   bomb: Phaser.GameObjects.Sprite;
   network: Network;
   enemies: Phaser.GameObjects.Sprite[];
   spacebar: any;
+  bombs: any;
+  bombCounter: number;
 
   constructor() {
     super({
@@ -22,14 +24,15 @@ class TestScene extends Phaser.Scene {
     this.load.image('player', '/assets/sprites/player.png');
     this.load.image('black-square', '/assets/sprites/black-square.png');
     this.load.image('bomb', '/assets/sprites/bomb.png');
-
   }
 
   create() {
-    this.player = this.physics.add.sprite(100, 100, 'player');
-    this.player.setCollideWorldBounds(true);
-    this.player.enableBody(true);
-    this.physics.world.enable(this.player);
+    this.player = new Manbomber({
+      scene: this,
+      x: 100,
+      y: 100,
+      key: "player"
+    });
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -40,21 +43,19 @@ class TestScene extends Phaser.Scene {
         this.squares.create(j, i, 'black-square');
       }
     }
-
-    this.bomb = this.add.sprite(100, 100, 'bomb');
-    this.bomb.visible = false;
-
+    this.bombs = this.physics.add.staticGroup();
+    this.bombCounter = 0;
     this.enemies = [];
     for (let i = 0; i < 10; i++) {
       this.enemies[i] = this.add.sprite(0, 0, 'player');
       this.enemies[i].visible = false;
 
     }
+
   }
 
   update(time: number, delta: number) {
     this.physics.add.collider(this.player, this.squares);
-
     this.network.update(time, this.player, this.enemies);
 
     if (this.cursors.left.isDown) {
@@ -70,11 +71,15 @@ class TestScene extends Phaser.Scene {
       this.player.y -= 5;
     }
 
-    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
-      this.bomb.visible = true;
-      this.bomb.x = this.player.x;
-      this.bomb.y = this.player.y;
-      console.log('DROP BOMB');
+    this.player.update();
+    if (this.cursors.space.justDown) {
+      if (this.player.canUseBomb()) {
+        debugger;
+        this.bombs.create(this.player.x, this.player.y, 'bomb');
+        console.log('DROP BOMB');
+        this.player.useBomb()
+      }
+
     }
   }
 }
