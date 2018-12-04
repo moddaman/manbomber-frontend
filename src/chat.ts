d395043f9c5c574d55324c8619e738468f7703b5
oystein.bjerland <oystein.bjerland@gmail.com>
2018-12-04 19:31:43 +0100

    Navn i chat

--- src/chat.ts
+++ src/chat.ts
@@ -1,6 +1,11 @@
 declare var window;
 import 'phaser';
-import {socket, SOCKET_CHAT_KEY} from './network/socket';
+import {getUrlParam, socket, SOCKET_CHAT_KEY} from './network/socket';
+
+interface NetworkMsg {
+  message: string;
+  name: string;
+}
 
 export class Chat {
   constructor() {
@@ -12,10 +17,12 @@ export class Chat {
     socket.on(SOCKET_CHAT_KEY, Chat.onRecieve);
   }
 
-  static onRecieve(msg: string) {
+  static onRecieve(msg: NetworkMsg) {
     const ul = document.getElementById("messages");
     const li = document.createElement("li");
-    li.appendChild(document.createTextNode(msg));
+
+    let text = `${msg.name}: ${msg.message}`;
+    li.appendChild(document.createTextNode(text));
     ul.appendChild(li);
 
   }
@@ -25,7 +32,11 @@ export class Chat {
     const message = input.value;
     input.value = '';
 
-    socket.emit(SOCKET_CHAT_KEY, message);
+    const msg: NetworkMsg = {
+      message,
+      name: getUrlParam('player')
+    };
+    socket.emit(SOCKET_CHAT_KEY, msg);
   }
 }
 
