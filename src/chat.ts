b875742940d715a6792972a86c4949224bb97c37
oystein.bjerland <oystein.bjerland@gmail.com>
2018-12-04 19:58:54 +0100

    Navn i chat

--- src/chat.ts
+++ src/chat.ts
@@ -1,6 +1,6 @@
 declare var window;
 import 'phaser';
-import {getUrlParam, socket, SOCKET_CHAT_KEY} from './network/socket';
+import {getUrlParam, socket, SOCKET_CHAT_KEY, SOCKET_SEND_CHAT_KEY} from './network/socket';
 
 interface NetworkMsg {
   message: string;
@@ -25,6 +25,7 @@ export class Chat {
     li.appendChild(document.createTextNode(text));
     ul.appendChild(li);
 
+    console.log("mottok", msg);
   }
 
   static sendMessage() {
@@ -32,11 +33,12 @@ export class Chat {
     const message = input.value;
     input.value = '';
 
+    console.log("send beskjed", message);
     const msg: NetworkMsg = {
       message,
       name: getUrlParam('player')
     };
-    socket.emit(SOCKET_CHAT_KEY, msg);
+    socket.emit(SOCKET_SEND_CHAT_KEY, msg);
   }
 }
 
