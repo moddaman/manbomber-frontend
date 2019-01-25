4ae7ca6442b002f5e0ee76392f6beeb62bba29f8
oystein.bjerland <oystein.bjerland@gmail.com>
2019-01-25 18:40:51 +0100

    typer

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,25 @@ export interface ExplotionRadius {
     x: number,
     y: number
 }
+
+
+interface GameState {
+  players: PlayerState[]
+  tiles: BombType[];
+}
+
+interface PlayerState {
+  x: number;
+  y: number;
+  name: string;
+  bombs: BombState[];
+
+}
+
+interface BombState {
+  x: number;
+  y: number;
+}
+
+type BombType = 'BLOCK' | 'BOX' | 'EMPTY' | 'SPEED'
+
